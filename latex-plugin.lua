a467b4d3a39c6ce69ca61c900d3080757d4e0f7d
chykcha <trollboy021@gmail.com>
2024-02-21 17:35:40 +0100

    Changed version to the 0.4.3
    Added URL to the archive with plugin to the repo.json file

--- latex-plugin.lua
+++ latex-plugin.lua
@@ -1,4 +1,4 @@
-VERSION = "0.4.2"
+VERSION = "0.4.3"
 
 
 local micro = import("micro")
@@ -25,9 +25,9 @@ function onBufferOpen(buf)
 	isTex = (buf:FileType() == "tex")
 	if isTex then
 		local fileName = buf:GetName()
-		local truncFileName =fileName:sub(1, -5)
-		local syncFileName = truncFileName .. ".synctex.from-zathura-to-micro"
-		local scriptFifoWriteFileName = truncFileName .. ".fifo-writer.sh"
+		-- local truncFileName =fileName:sub(1, -5)
+		local syncFileName = fileName .. ".synctex.from-zathura-to-micro"
+		local scriptFifoWriteFileName = fileName .. ".fifo-writer.sh"
 		local scriptFifoWrite = "echo \"$@\" > " .. syncFileName:gsub(" ", "\\%0")
 		local scriptFifoRead = "while true;do if read line; then echo $line; fi;sleep 0.5; done < " .. syncFileName:gsub(" ", "\\%0")
 		
@@ -64,13 +64,13 @@ end
 
 function synctexForward(bp)
 	local fileName = bp.Buf:GetName():gsub(" ", "\\%0")
-	local truncFileName = fileName:sub(1, -5)
-	local syncFileName = truncFileName .. ".synctex.from-zathura-to-micro"
-	local scriptFifoWriteFileName = truncFileName .. ".fifo-writer.sh"
-	local pdfFileName = truncFileName .. ".pdf"
+	-- local truncFileName = fileName:sub(1, -5)
+	local syncFileName = fileName .. ".synctex.from-zathura-to-micro"
+	local scriptFifoWriteFileName = fileName .. ".fifo-writer.sh"
+	local pdfFileName = fileName:sub(1, -5) .. ".pdf"
 
 	local cursor = bp.Buf:GetActiveCursor()
-	local zathuraArgPos = string.format(" --synctex-forward=%i:%i:%s", cursor.Y + 1, cursor.X + 1, fileName)
+	local zathuraArgPos = string.format(" --synctex-forward=%i:%i:%s", cursor.Y + 1, cursor.X, fileName)
 	local zathuraArgSynctexBackward = " --synctex-editor-command=\'" .. scriptFifoWriteFileName .." %{line}\'"
 	local zathuraArgFile = " " .. pdfFileName;
 
@@ -87,10 +87,10 @@ end
 
 function lint(bp)
 	local fileName = bp.Buf:GetName()
-	local truncFileName = fileName:sub(1, -5)
+	-- local truncFileName = fileName:sub(1, -5)
 
 	-- syncex=15 added because otherwise pdflatex cleans up synctex files as well
-	local output = shell.ExecCommand("pdflatex", "-synctex=15", "-interaction=nonstopmode", "-draftmode", "-file-line-error", truncFileName)
+	local output = shell.ExecCommand("pdflatex", "-synctex=15", "-interaction=nonstopmode", "-draftmode", "-file-line-error", fileName)
 	local error = output:match("[^\n/]+:%w+:[^\n]+")
 	if error then
 		micro.InfoBar():Message(error)
@@ -105,20 +105,20 @@ end
 
 function compile(bp)
 	local fileName = bp.Buf:GetName()
-	local truncFileName =fileName:sub(1, -5)
+	-- local truncFileName =fileName:sub(1, -5)
 	
 	-- shell.RunCommand("bibtex " .. truncFileName)
-	shell.ExecCommand("pdflatex", "-synctex=15", "-interaction=nonstopmode", "-draftmode", truncFileName)
-	shell.ExecCommand("pdflatex", "-synctex=15", "-interaction=nonstopmode", truncFileName)
+	shell.ExecCommand("pdflatex", "-synctex=15", "-interaction=nonstopmode", "-draftmode", fileName)
+	shell.ExecCommand("pdflatex", "-synctex=15", "-interaction=nonstopmode", fileName)
 end
 
 
 function preQuit(bp)
 	if isTex then
 		local fileName = bp.Buf:GetName()
-		local truncFileName = fileName:sub(1, -5)
-		local syncFileName = truncFileName .. ".synctex.from-zathura-to-micro"
-		local scriptFifoWriteFileName = truncFileName .. ".fifo-writer.sh"
+		-- local truncFileName = fileName:sub(1, -5)
+		local syncFileName = fileName .. ".synctex.from-zathura-to-micro"
+		local scriptFifoWriteFileName = fileName .. ".fifo-writer.sh"
 
 		shell.JobStop(jobFifoRead)
 		shell.ExecCommand("rm", syncFileName)
